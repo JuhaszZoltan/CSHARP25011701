80f93d01fbd66dc4386b04d6aecc06ee24910fc8
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-01-17 10:26:05 +0100

    Add project files.

---
+++ NepessegCLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using NepessegCLI;
+using System.Text;
+
+const string PATH = "C:\\Users\\juhaszz\\source\\repos\\CSHARP25011701\\NEPESSEG\\adatok-utf8.txt";
+List<Orszag> orszagok = [];
+
+using StreamReader sr = new(PATH, Encoding.UTF8);
+_ = sr.ReadLine();
+while(!sr.EndOfStream)
+{
+    string sor = sr.ReadLine();
+    string[] darabok = sor.Split(';');
+
+    string orszagNev = darabok[0];
+    int terulet = int.Parse(darabok[1]);
+
+    //string nepesegAdat = darabok[2];
+    //int nepessegSzam = 0;
+    //if (nepesegAdat[^1] == 'g')
+    //{
+    //    nepessegSzam = int.Parse(nepesegAdat[0..^1]) * 10000;
+    //}
+    //else
+    //{
+    //    nepessegSzam = int.Parse(nepesegAdat);
+    //}
+
+    // változó = <feltétel>
+    // ? <érték, ha a feltétel igaz>
+    // : <érték, ha a feltétel hamis>
+
+    //int nepesseg = int.Parse(darabok[2]);
+
+    int nepesseg = darabok[2][^1] == 'g'
+        ? int.Parse(darabok[2][..^1]) * 10000
+        : int.Parse(darabok[2]);
+
+}
+
+
+
